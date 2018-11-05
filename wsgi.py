1a30e708398ab22361bb179c1a267016e7e2e6de
joe <joeldrt@gmail.com>
2018-11-05 16:36:51 -0600

    agregando el ini de uwsgi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
-from remanager_back.app import main
+from remanager_back.app import app, initialize_app, log, settings
 
 
 if __name__ == '__main__':
-    main()
+    initialize_app(app)
+    log.info('>>>>>>>> Iniciando servidor de desarrollo en http://{}/api/ <<<<<<<<<'.format(app.config['SERVER_NAME']))
+    app.run(debug=settings.FLASK_DEBUG)
