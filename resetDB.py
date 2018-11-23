b3a30e0a4b6b85870f5c12f008ad943eae736cff
joe <joeldrt@gmail.com>
2018-11-22 23:52:21 -0600

    agregando el ini de uwsgi

--- resetDB.py
+++ resetDB.py
@@ -1,6 +1,8 @@
 from app import initialize_app, app
 from remanager_back.data_auth import reset_database
 
-initialize_app(app)
+# se comenta la siguiente linea porque ya app.py hace la inicialización de la app
+# initialize_app(app)
+
 with app.app_context():
     reset_database()
